ff0d5771e804832cc2faa6e3d7314fe666a40447
apalahina <palagian666@gmail.com>
2023-07-25 23:14:41 +0300

    Django_HW5

--- Django_HW/create/forms.py
+++ Django_HW/create/forms.py
@@ -1,13 +1,16 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
 from users.models import User
 from books.models import Book
 from purchases.models import Purchase
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'age', 'email', 'password']
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + ('age', )
 
 
 class BookForm(forms.ModelForm):
