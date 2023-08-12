53c1ebd8c1b98b9c7167b150cc4e414274ce32c1
apalahina <palagian666@gmail.com>
2023-08-12 17:34:33 +0300

    Django_HW7_v3

--- Django_HW/users/views.py
+++ Django_HW/users/views.py
@@ -11,3 +11,9 @@ class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     context_object_name = 'user'
+
+
+def user_list(request):
+    users = User.objects.all()
+    user_ids = [user.id for user in users]
+    print_users_purchases.delay(user_ids)
